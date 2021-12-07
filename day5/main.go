95983a502ac8137971e2e6afd8ea7249d309d01d
Dewi Williams <dewi@dewiwilliams.co.uk>
2021-12-07 19:17:49 +0000

    Day 5 part 2

--- day5/main.go
+++ day5/main.go
@@ -20,14 +20,12 @@ func (l *line) isHorizontal() bool {
 func (l *line) isVertical() bool {
 	return l.start[0] == l.end[0]
 }
+func (l *line) isDiagonal() bool {
+	return l.start[0] != l.end[0] && l.start[1] != l.end[1]
+}
 func (l *line) applyHorizontalLineToBoard(board []int, stride int) {
 	startx := l.start[0]
 	endx := l.end[0]
-
-	if startx > endx {
-		startx = l.end[0]
-		endx = l.start[0]
-	}
 
 	for x := startx; x <= endx; x++ {
 		board[x+l.start[1]*stride]++
@@ -37,20 +35,40 @@ func (l *line) applyVerticalLineToBoard(board []int, stride int) {
 	starty := l.start[1]
 	endy := l.end[1]
 
-	if starty >= endy {
-		starty = l.end[1]
-		endy = l.start[1]
-	}
-
 	for y := starty; y <= endy; y++ {
 		board[l.start[0]+y*stride]++
 	}
 }
-func (l *line) applyLineToBoard(board []int, stride int) {
+func (l *line) applyDiagonal(board []int, stride int) {
+	step := 1
+	if l.end[1] < l.start[1] {
+		step = -1
+	}
+
+	startx := l.start[0]
+	endx := l.end[0]
+	limit := endx - startx
+	starty := l.start[1]
+
+	for i := 0; i <= limit; i++ {
+		x := startx + i
+		y := starty + i*step
+
+		board[x+y*stride]++
+	}
+}
+func (l *line) applyLineToBoard(board []int, stride int, applyDiagonals bool) {
 	if l.isHorizontal() {
 		l.applyHorizontalLineToBoard(board, stride)
 	} else if l.isVertical() {
 		l.applyVerticalLineToBoard(board, stride)
+	} else if l.isDiagonal() {
+		if applyDiagonals {
+			l.applyDiagonal(board, stride)
+		}
+	} else {
+		fmt.Println("Unrecognised line state")
+		os.Exit(2)
 	}
 }
 
@@ -58,6 +76,7 @@ func main() {
 	data := getData()
 
 	part1(data)
+	part2(data)
 }
 
 func part1(lines []line) {
@@ -68,12 +87,24 @@ func part1(lines []line) {
 	boardState := make([]int, maximums[0]*maximums[1])
 
 	for _, line := range lines {
-		line.applyLineToBoard(boardState, maximums[0])
+		line.applyLineToBoard(boardState, maximums[0], false)
 	}
 
-	fmt.Printf("Overlap points: %d\n", countOverlapPoints(boardState))
+	fmt.Printf("Part 1 points: %d\n", countOverlapPoints(boardState))
 }
+func part2(lines []line) {
+	maximums := getBoardMaximums(lines)
+	maximums[0]++
+	maximums[1]++
 
+	boardState := make([]int, maximums[0]*maximums[1])
+
+	for _, line := range lines {
+		line.applyLineToBoard(boardState, maximums[0], true)
+	}
+
+	fmt.Printf("PArt 2 points: %d\n", countOverlapPoints(boardState))
+}
 func countOverlapPoints(board []int) int {
 	result := 0
 
@@ -123,8 +154,23 @@ func getData() []line {
 		fields := strings.Fields(scanner.Text())
 
 		line := line{}
-		line.start = parseCoordinate(fields[0])
-		line.end = parseCoordinate(fields[2])
+
+		p1 := parseCoordinate(fields[0])
+		p2 := parseCoordinate(fields[2])
+
+		if p1[0] < p2[0] {
+			line.start = p1
+			line.end = p2
+		} else if p1[0] > p2[0] {
+			line.start = p2
+			line.end = p1
+		} else if p1[1] < p2[1] {
+			line.start = p1
+			line.end = p2
+		} else {
+			line.start = p2
+			line.end = p1
+		}
 
 		result = append(result, line)
 	}
