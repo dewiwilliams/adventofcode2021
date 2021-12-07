995675934deb732f3713d7466b460af904a7233f
Dewi Williams <dewi@dewiwilliams.co.uk>
2021-12-07 20:21:52 +0000

    Day 7 part 2

--- day7/main.go
+++ day7/main.go
@@ -11,19 +11,34 @@ import (
 
 func main() {
 	data := getData()
-	//fmt.Printf("Got data: %v\n", data)
 
 	part1(data)
-	//part2(data)
+	part2(data)
 }
 
-func part1(values []int) {
-	minimum := calculateFuel(values, 0)
+func part2(values []int) {
+	minimum := calculateGeometericFuelTotal(values, 0)
 
 	limit := max(values)
 
 	for i := 1; i < limit; i++ {
-		cost := calculateFuel(values, i)
+		cost := calculateGeometericFuelTotal(values, i)
+		if cost < minimum {
+			minimum = cost
+		}
+	}
+
+	fmt.Printf("Part 2 fuel cost: %d\n", minimum)
+}
+func part1(values []int) {
+	// Instead of linearly scanning the full search space, start at the median
+	// and search either side from there until the cost is increasing.
+	minimum := calculateLinearFuelTotal(values, 0)
+
+	limit := max(values)
+
+	for i := 1; i < limit; i++ {
+		cost := calculateLinearFuelTotal(values, i)
 		if cost < minimum {
 			minimum = cost
 		}
@@ -31,8 +46,20 @@ func part1(values []int) {
 
 	fmt.Printf("Part 1 fuel cost: %d\n", minimum)
 }
+func calculateGeometericFuelTotal(values []int, position int) int {
+	result := 0
 
-func calculateFuel(values []int, position int) int {
+	for _, value := range values {
+		result += calculateGeometricFuelCost(abs(value - position))
+	}
+
+	return result
+}
+func calculateGeometricFuelCost(distance int) int {
+	//Sum of arithmetic sequence
+	return ((distance + 1) * distance) / 2
+}
+func calculateLinearFuelTotal(values []int, position int) int {
 	result := 0
 
 	for _, value := range values {
