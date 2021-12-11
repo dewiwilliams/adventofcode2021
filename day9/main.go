6a56c71537b22e02c2af9bae5b2185f2fd212834
Dewi Williams <dewi@dewiwilliams.co.uk>
2021-12-11 20:43:58 +0000

    Day 9 part 2

--- day9/main.go
+++ day9/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -12,19 +13,35 @@ import (
 func main() {
 	grid, width := getData()
 
-	fmt.Printf("Got data: %v\n", grid)
-	fmt.Printf("Got width: %d\n", width)
-
 	part1(grid, width)
+	part2(grid, width)
 }
 func part1(grid []int, width int) {
 	fmt.Printf("Part 1 risk level: %d\n", getRiskLevel(grid, width))
 }
+func part2(grid []int, width int) {
+	basinSizes := getBasinSizes(grid, width)
+	sort.Ints(basinSizes)
+
+	largestBasins := basinSizes[len(basinSizes)-3:]
+	result := largestBasins[0] * largestBasins[1] * largestBasins[2]
+
+	fmt.Printf("Part 2 result: %d\n", result)
+}
 func getRiskLevel(grid []int, width int) int {
-	height := len(grid) / width
 	risklevel := 0
 
-	fmt.Printf("Got height: %d\n", height)
+	lowPoints := getLowPoints(grid, width)
+	for _, point := range lowPoints {
+		risklevel += grid[point] + 1
+	}
+
+	return risklevel
+}
+func getLowPoints(grid []int, width int) []int {
+	result := []int{}
+
+	height := len(grid) / width
 
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
@@ -43,11 +60,76 @@ func getRiskLevel(grid []int, width int) int {
 				continue
 			}
 
-			risklevel += grid[index] + 1
+			result = append(result, index)
 		}
 	}
 
-	return risklevel
+	return result
+}
+func getBasinSizes(grid []int, width int) []int {
+	result := []int{}
+
+	lowPoints := getLowPoints(grid, width)
+	for _, point := range lowPoints {
+		result = append(result, getBasinSize(grid, width, point))
+	}
+
+	return result
+}
+func getBasinSize(grid []int, width, lowpoint int) int {
+	pointsToProcess := map[int]bool{}
+	pointsToProcess[lowpoint] = true
+
+	height := len(grid) / width
+
+	pointsInBasin := map[int]bool{}
+	pointsInBasin[lowpoint] = true
+
+	for len(pointsToProcess) > 0 {
+		key := getFirstKey(pointsToProcess)
+		delete(pointsToProcess, key)
+
+		neighbours := getNeighbours(key, width, height)
+		for _, neighbour := range neighbours {
+			if grid[neighbour] == 9 {
+				continue
+			}
+			if grid[neighbour] > grid[key] {
+				pointsInBasin[neighbour] = true
+				pointsToProcess[neighbour] = true
+			}
+		}
+
+	}
+
+	return len(pointsInBasin)
+}
+func getFirstKey(m map[int]bool) int {
+	for k := range m {
+		return k
+	}
+	return -1
+}
+func getNeighbours(point, width, height int) []int {
+	result := []int{}
+
+	x := point % width
+	y := point / width
+
+	if x > 0 {
+		result = append(result, point-1)
+	}
+	if x < width-1 {
+		result = append(result, point+1)
+	}
+	if y > 0 {
+		result = append(result, point-width)
+	}
+	if y < height-1 {
+		result = append(result, point+width)
+	}
+
+	return result
 }
 func getData() ([]int, int) {
 	result := []int{}
--- day9/main_test.go
+++ day9/main_test.go
@@ -18,3 +18,33 @@ func TestPart1(t *testing.T) {
 		t.Errorf("Wrong risk level: %d vs %d", risklevel, 15)
 	}
 }
+
+func TestPart2(t *testing.T) {
+	testData := []int{
+		2, 1, 9, 9, 9, 4, 3, 2, 1, 0,
+		3, 9, 8, 7, 8, 9, 4, 9, 2, 1,
+		9, 8, 5, 6, 7, 8, 9, 8, 9, 2,
+		8, 7, 6, 7, 8, 9, 6, 7, 8, 9,
+		9, 8, 9, 9, 9, 6, 5, 6, 7, 8,
+	}
+
+	basin1Size := getBasinSize(testData, 10, 1)
+	if basin1Size != 3 {
+		t.Errorf("Wrong basin 1 size: %d vs %d", basin1Size, 3)
+	}
+
+	basin2Size := getBasinSize(testData, 10, 9)
+	if basin2Size != 9 {
+		t.Errorf("Wrong basin 2 size: %d vs %d", basin2Size, 9)
+	}
+
+	basin3Size := getBasinSize(testData, 10, 22)
+	if basin3Size != 14 {
+		t.Errorf("Wrong basin 3 size: %d vs %d", basin3Size, 14)
+	}
+
+	basin4Size := getBasinSize(testData, 10, 46)
+	if basin4Size != 9 {
+		t.Errorf("Wrong basin 4 size: %d vs %d", basin4Size, 9)
+	}
+}
